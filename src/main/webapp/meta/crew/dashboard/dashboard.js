d3df1f23d7d5dffc859f608798e743b15d83b873
Thomas <f.t.vandervanderboon@student.utwente.nl>
2023-06-07 20:44:06 +0200

    Commit to merge

--- src/main/webapp/meta/crew/dashboard/dashboard.js
+++ src/main/webapp/meta/crew/dashboard/dashboard.js
@@ -25,3 +25,57 @@ function setTheme(href){
 function reloadEvent() {
     //TODO: figure out a way to call the current event page
 }
+
+
+
+/*Filter*/
+
+
+function parseCookie(cookieString) {
+    const cookies = {};
+    cookieString.split(';').forEach(cookie => {
+        const [name, value] = cookie.trim().split('=');
+        cookies[name] = decodeURIComponent(value);
+    });
+    return cookies;
+}
+
+
+function updateBookings(filterType) {
+
+    const bookingWidget = document.createElement("div");
+    bookingWidget.setAttribute("class","booking");
+    bookingWidget.setAttribute("id", "booking_");
+
+    var filter;
+
+    switch (filterType) {
+        case 'in_progress':
+            filter = "ongoing";
+            break;
+        case 'future':
+            filter = "ongoing";
+            break;
+        case 'past':
+            filter = "past";
+            break;
+        default:
+            filter = 'ongoing';
+    }
+
+    const cookies = parseCookie(document.cookie);
+    const id = cookies['account_id'];
+
+    const url = `http://localhost:8080/shotmaniacs2/api/crew/${id}/mybooking?filtertime=${filter}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(booking => {
+                console.log(booking.name);
+            });
+        })
+        .catch(error => {
+            // Handle any errors
+            console.error(error);
+        });
+}
