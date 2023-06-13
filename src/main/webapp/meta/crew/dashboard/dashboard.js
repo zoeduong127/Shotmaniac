ad475bc06c1a6231bf7cf8c96dc3fcb708e4cadd
Luca Fuertes <lucafuertes04@gmail.com>
2023-06-13 23:00:43 +0200

    Added search functionality to dashboard.html

--- src/main/webapp/meta/crew/dashboard/dashboard.js
+++ src/main/webapp/meta/crew/dashboard/dashboard.js
@@ -32,17 +32,6 @@ function reloadEvent() {
 const bookingContainer = document.getElementById("booking_container");
 const bookingElement = bookingContainer.firstElementChild.cloneNode(true);
 
-function reloadCss()
-{
-    var links = document.getElementsByTagName("link");
-    for (var cl in links)
-    {
-        var link = links[cl];
-        if (link.rel === "stylesheet")
-            link.href += "";
-    }
-}
-
 function parseCookie(cookieString) {
     const cookies = {};
     cookieString.split(';').forEach(cookie => {
@@ -52,67 +41,17 @@ function parseCookie(cookieString) {
     return cookies;
 }
 
+function performQueryAndUpdateBookings(url) {
 
-function updateBookings(filterType) {
     while (bookingContainer.firstChild) {
         bookingContainer.removeChild(bookingContainer.firstChild);
     }
 
-    filterButton = document.getElementById("filter_button_text");
-
-    var filter;
-
-    switch (filterType) {
-        case 'in_progress':
-            filter = "ongoing";
-            filterButton.textContent = "Filter: In progress";
-            break;
-        case 'future':
-            filter = "ongoing";
-            filterButton.textContent = "Filter: On going";
-            break;
-        case 'past':
-            filter = "past";
-            filterButton.textContent = "Filter: Past";
-            break;
-        default:
-            filter = 'ongoing';
-    }
-
-
-
-    const cookies = parseCookie(document.cookie);
-    const id = cookies['account_id'];
-
-    const url = `http://localhost:8080/shotmaniacs2/api/crew/${id}/mybooking/timefilter/${filter}`;
     fetch(url)
         .then(response => response.json())
         .then(data => {
             data.forEach(booking => {
-
-                // const bookingWidget = document.createElement("div");
-                // bookingWidget.setAttribute("class","booking");
-                // bookingWidget.setAttribute("id", "booking_" + booking.id);
-                // const eventName = document.createElement("p");
-                // eventName.innerHTML = booking.name;
-                // bookingWidget.appendChild(eventName);
-                //
-                // rightInnerBooking = document.createElement("div");
-                // rightInnerBooking.setAttribute("class", "innerBooking right");
-                // const date = document.createElement("p");
-                // dateTime = new Date(booking.date);
-                // date.innerText = dateTime.toDateString() + ', ' + dateTime.getHours() + ':' + dateTime.getMinutes();
-                // rightInnerBooking.appendChild(date);
-                // bookingWidget.appendChild(rightInnerBooking);
-                //
-                // leftInnerBooking = document.createElement("div");
-                // leftInnerBooking.setAttribute("class", "innerBooking left");
-                // statusText = document.createElement("p");
-                // statusText.innerText = "TODO"
-                // leftInnerBooking.appendChild(statusText);
-                // bookingWidget.appendChild(leftInnerBooking);
-
-                var bookingElementCopy = bookingElement.cloneNode(true);
+                let bookingElementCopy = bookingElement.cloneNode(true);
 
                 //TODO: Calculate amount of slots already taken.
                 bookingElementCopy.querySelector("#event_name").innerHTML = booking.name + " <span class=\"bolded\">(" + booking.slots + ")</span>";
@@ -154,3 +93,51 @@ function updateBookings(filterType) {
             console.error(error);
         });
 }
+
+
+function updateBookings(filterType) {
+    const filterButton = document.getElementById("filter_button_text");
+
+    var filter;
+
+    switch (filterType) {
+        case 'in_progress':
+            filter = "ongoing";
+            filterButton.textContent = "Filter: In progress";
+            break;
+        case 'future':
+            filter = "ongoing";
+            filterButton.textContent = "Filter: On going";
+            break;
+        case 'past':
+            filter = "past";
+            filterButton.textContent = "Filter: Past";
+            break;
+        default:
+            filter = 'ongoing';
+
+    }
+
+    const cookies = parseCookie(document.cookie);
+    const id = cookies['account_id'];
+
+    const url = `http://localhost:8080/shotmaniacs2/api/crew/${id}/mybooking/timefilter/${filter}`;
+
+    performQueryAndUpdateBookings(url);
+
+}
+
+function searchBookings(searchText) {
+    const cookies = parseCookie(document.cookie);
+    const id = cookies['account_id'];
+    const url = `http://localhost:8080/shotmaniacs2/api/crew/${id}/mybooking/search?searchtext=\"${searchText}\"`;
+    performQueryAndUpdateBookings(url);
+}
+
+//Event listeners
+const searchbox = document.getElementById("search-input");
+searchbox.addEventListener("keydown", function (e) {
+    if (e.code === "Enter") {  //checks whether the pressed key is "Enter"
+        searchBookings(searchbox.value);
+    }
+});
