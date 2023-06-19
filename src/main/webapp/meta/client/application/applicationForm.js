e56b52800b95a7bd8873e4624e927aa15b5ac5eb
Thomas <f.t.vandervanderboon@student.utwente.nl>
2023-06-19 18:50:47 +0200

    mid-work commit to restart computer

---
+++ src/main/webapp/meta/client/application/applicationForm.js
@@ -0,0 +1,9 @@
+//Did add it into html
+/*
+const actualBtn = document.getElementById('actual-btn');
+
+const fileChosen = document.getElementById('file-chosen');
+
+actualBtn.addEventListener('change', function(){
+    fileChosen.textContent = this.files[0].name
+})*/
