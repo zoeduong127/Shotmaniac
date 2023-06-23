a286ac9716960b700fcb83a3bf09604df613f8b3
Thomas <f.t.vandervanderboon@student.utwente.nl>
2023-06-23 09:43:25 +0200

    mid-work commit, huyen's account now redirects to admin dashboard

--- src/main/webapp/meta/crew/news/notify.js
+++ src/main/webapp/meta/crew/news/notify.js
@@ -2,6 +2,8 @@ const announcementListItems = document.querySelectorAll(".announcement-list-item
 const announcementContent = document.querySelector(".announcement-content");
 const announcementTitle = document.querySelector("#announcement-title");
 const announcementDescription = document.querySelector("#announcement-description");
+const announcementList = document.querySelector('.announcement-list');
+
 //clicking on an announcement makes it show the content
 announcementListItems.forEach(function(item) {
     item.addEventListener("click", function() {
@@ -10,17 +12,22 @@ announcementListItems.forEach(function(item) {
 
         announcementTitle.textContent = title;
         announcementDescription.textContent = content;
+        announcementContent.style.display = "block";
     });
 });
 
-announcementContent.addEventListener('click', function() {
+/*announcementContent.addEventListener('click', function() {
     announcementTitle.textContent = '';
     announcementDescription.textContent = '';
-});
+});*/
 
 
 const filterOptions = document.querySelectorAll('.filter-option');
 const filterButton = document.querySelector('.filter-button');
+
+filterButton.addEventListener('click', function() {
+    announcementList.classList.toggle('announcement-list-filtered');
+});
 
 filterOptions.forEach(function(option) {
     option.addEventListener('click', function(e) {
@@ -31,3 +38,23 @@ filterOptions.forEach(function(option) {
         // You can add your code here to filter the announcements based on the selected option
     });
 });
+
+const addCommentButton = document.getElementById("add-comment");
+const commentBox = document.getElementById("comment-box");
+const commentInput = document.getElementById("comment-input");
+const commentSubmitButton = document.getElementById("comment-submit");
+
+addCommentButton.addEventListener("click", function() {
+    commentBox.style.display = "block";
+});
+
+commentSubmitButton.addEventListener("click", function() {
+    const comment = commentInput.value.trim();
+    if (comment !== "") {
+        // Logic to handle the submitted comment
+        console.log("Submitted comment:", comment);
+        commentInput.value = "";
+        commentBox.style.display = "none";
+    }
+});
+
