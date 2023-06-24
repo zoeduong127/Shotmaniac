4bce0caa702a47506ff09d73f41708eea9a15107
Luca Fuertes <lucafuertes04@gmail.com>
2023-06-24 08:59:21 +0200

    Revert "remove /target"
    This reverts commit a9d4a5f8e7c9b5924d8cffc3e0c1b1a997ca7e24.

---
+++ target/shotmaniacs2/meta/crew/calendar/calendar.js
@@ -0,0 +1,231 @@
+let date = new Date();
+const event_info = document.getElementById("event_container");
+const calendar = document.getElementById("calendar_container");
+let correctDay= [];
+let blob = "";
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+
+function renderCalendar(){
+    date.setDate(1); //sets the date to the first day of the month (used for previous days)
+
+    const daysInMonth = document.querySelector(".days");
+
+    const lastDay = new Date(date.getFullYear(),
+        date.getMonth() + 1, 0).getDate();
+
+    const prevLastDay = new Date(date.getFullYear(),
+        date.getMonth(), 0).getDate();
+
+    const fistDayIndex = date.getDay();
+
+    const lastDayIndex = new Date(date.getFullYear(),
+        date.getMonth() + 1, 0).getDay();
+
+    const nextDays = 7 - lastDayIndex - 1;
+
+
+    document.querySelector(".date h1").innerHTML
+        = `${months[date.getMonth()]}` + " " + date.getFullYear();
+
+        document.querySelector(".date p").innerHTML
+            = new Date().toDateString();
+
+
+    let days = "";
+    for (let offset = fistDayIndex; offset > 0; offset--) { //days of the previous month
+        days += `<div class = "prev-date" onclick="performQueryAndUpdateBookings(this)">${prevLastDay - offset + 1} </div>`
+    }
+
+    for (let i = 1; i <= lastDay; i++) { //days of the month
+        if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+            days += `<div class="today" onclick="performQueryAndUpdateBookings(this)">${i}</div>`;
+        } else {
+            if (i < 10) {
+                days += `<div class="day" onclick="performQueryAndUpdateBookings(this)">0${i}</div>`
+            } else {
+                days += `<div class = "day" onclick="performQueryAndUpdateBookings(this)">${i}</div>`;
+            }
+        }
+    }
+
+    for (let offset = 1; offset <= nextDays; offset++) { //days of next month
+        days += `<div class="next-date" onclick="performQueryAndUpdateBookings(this)">0${offset}</div>`
+    }
+    daysInMonth.innerHTML = days;
+}
+
+//arrow movement
+document.querySelector('.prev').addEventListener('click', () => {
+    date.setMonth(date.getMonth() - 1);
+    renderCalendar();
+});
+
+document.querySelector('.next').addEventListener('click', () => {
+    date.setMonth(date.getMonth() + 1);
+    renderCalendar();
+});
+
+function returnToDay() {
+    date = new Date();
+    console.log()
+    renderCalendar();
+}
+function back() {
+    event_info.style.visibility = "hidden";
+
+    calendar.style.opacity = "1";
+    calendar.style.zIndex = "auto";
+
+
+    correctDay = [];
+    blob = "";
+}
+/*Fetching backend information*/
+function parseCookie(cookieString) {
+    const cookies = {};
+    cookieString.split(';').forEach(cookie => {
+        const [name, value] = cookie.trim().split('=');
+        cookies[name] = decodeURIComponent(value);
+    });
+    return cookies;
+}
+
+function performQueryAndUpdateBookings(element) {
+    /*Styling*/
+    event_info.style.visibility = "visible";
+
+    calendar.style.opacity = "0.2";
+    calendar.style.zIndex = "0";
+
+
+
+    const cookies = parseCookie(document.cookie);
+    const id = cookies['account_id'];
+
+    const url = `http://localhost:8080/shotmaniacs2/api/crew/${id}/allbookings`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(booking => {
+                console.log("inner loop started")
+                let list = [booking];
+                console.log(list);
+
+
+                let BackDay = new Date(booking.date).getDate();
+                let BackMonth = new Date(booking.date).getMonth();
+                let BackYear = new Date(booking.date).getFullYear();
+
+                let CalDay = Array.from(element.innerHTML).slice(0, 2).join("");  //returns the date of the clicked event,
+                // complicated, due to the do icons added later.
+                let CalMonth = date.getMonth();
+                let CalYear = date.getFullYear();
+
+
+                if (BackDay == CalDay && BackMonth == CalMonth && BackYear == CalYear) {
+                    correctDay.push(booking);
+                    console.log(booking + " added to the correct day list")
+                }
+
+
+                /*
+                TODO: should add blobs, add this to the days in the calendar
+                console.log(Array.from(element.innerHTML).slice(0,2).join(""));
+                if (BackDay === CalDay && BackMonth === CalMonth && BackYear === CalYear) {
+                    const blobDiv = document.getElementById("blob" + Array.from(element.innerHTML).slice(0,2).join(""));
+                    blobDiv.innerHTML += `<i class="fas fa-period dot"></i`;
+                }*/
+
+
+            });
+            console.log("outer loop started")
+            if (correctDay.length === 0) {
+                console.log("list empty")
+                document.getElementById("blob-nav").innerHTML =
+                    `<span id="back" onclick="back()"> <img id="back-img" src="./../../Brand_Identity/images/icons/X-icon.png" alt="back">
+                        </span>`
+                    +
+                    `<span> No events available for this day</span>`;
+
+                document.getElementById("enroll-button").style.backgroundColor = "gray";
+
+            } else {
+                for (let i = 0; i < correctDay.length; i++) {
+                    blob += `<span class = "dots" onclick="displayInformation(correctDay.at(${i}))">${i + 1}</span>`
+                    console.log("blob added (" + i + ")")
+                }
+                document.getElementById("blob-nav").innerHTML = `<span id="back" onclick="back()"> 
+                         <img id="back-img" src="./../../Brand_Identity/images/icons/X-icon.png" alt="back">
+                                </span>` + blob;
+            }
+        })
+        .catch(error => {
+            // Handle any errors
+            console.error(error);
+        });
+}
+
+function displayInformation(booking) {
+    console.log(booking);
+
+    document.getElementById("event-name").innerHTML =
+        `<p>Name: <span>${booking.name}</span></p>`;
+
+    document.getElementById("event-type").innerHTML =
+        `<p>Event Type: <span>${booking.eventType}</span></p>`;
+
+    document.getElementById("date").innerHTML =
+        `<p>Date: <span>${
+                new Date(booking.date).getDate() + " " +
+                months[new Date(booking.date).getMonth()] + " " +
+                new Date(booking.date).getFullYear()
+    }</span></p>`;
+
+    document.getElementById("location").innerHTML =
+        `<p>Location: <span>${booking.location}</span></p>
+`;
+    document.getElementById("duration").innerHTML =
+        `<p>Duration: <span>${booking.duration}</span></p>`;
+
+    document.getElementById("booking-type").innerHTML =
+        `<p>Booking Type: <span>${booking.bookingType}</span></p>`;
+
+    document.getElementById("product-Manager").innerHTML =
+        `<p>Product Manager: <span>PRODUCT MANAGER FIELD NEEDED??</span></p>`;
+
+    document.getElementById("crew").innerHTML =
+        `<p>Crew: <span><ul>
+            <li>Name 1</li>
+            <li>Name 2</li>
+            <li>Name 3</li>
+            <li>Obscenely long name for testing purposes</li>
+            <li>Name 5</li>
+            </ul>
+            </span>
+          </p>`;
+}
+
+function enroll() {
+
+}
+
+
+/*Calls the functions that need to be loaded on their own*/
+renderCalendar();
+
---
+++ target/shotmaniacs2/meta/crew/dashboard/dashboard.js
@@ -0,0 +1,147 @@
+let currentTheme = document.getElementById("currentCSS");
+currentTheme.setAttribute('href', 'crewDashboardStyles.css');
+
+console.log("initial theme: " + currentTheme.getAttribute('href'))
+
+function toggleStyle() {
+    let theme = document.getElementsByTagName('link');
+    console.log(theme)
+
+    if (currentTheme.getAttribute("href") === 'crewDashboardStyles.css') {
+        currentTheme.setAttribute('href', 'singleEventHomePage.css');
+        console.log('new theme changed to: ' + currentTheme.getAttribute('href'));
+    } else {
+        currentTheme.setAttribute('href', 'crewDashboardStyles.css');
+        console.log('new theme changed to: ' + currentTheme.getAttribute('href'));
+    }
+}
+
+function setTheme(href){
+    currentTheme.setAttribute('href', href);
+}
+
+
+function reloadEvent() {
+    //TODO: figure out a way to call the current event page
+}
+
+
+
+/*Filter*/
+const bookingContainer = document.getElementById("booking_container");
+const bookingElement = bookingContainer.firstElementChild.cloneNode(true);
+
+function parseCookie(cookieString) {
+    const cookies = {};
+    cookieString.split(';').forEach(cookie => {
+        const [name, value] = cookie.trim().split('=');
+        cookies[name] = decodeURIComponent(value);
+    });
+    return cookies;
+}
+
+
+function    performQueryAndUpdateBookings(url) {
+
+    while (bookingContainer.firstChild) {
+        bookingContainer.removeChild(bookingContainer.firstChild);
+    }
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(booking => {
+                let bookingElementCopy = bookingElement.cloneNode(true);
+
+                //TODO: Calculate amount of slots already taken.
+
+                //TODO: the line below probably allows stored code attacks. Needs fixing
+                bookingElementCopy.querySelector("#event_name").innerHTML = booking.name /*+ " <span class=\"bolded\">(Available Slots: " + booking.slots + ")</span>"*/;
+                // * bookingElementCopy.querySelector("#booking_type").innerHTML = "<b>Booking Type: </b>" + booking.bookingType;
+
+                bookingElementCopy.querySelector("#location").innerHTML =  "<b>Location: </b>" + booking.location;
+//                bookingElementCopy.querySelector("#client").innerHTML =  "<b>Client: </b>" + booking.clientName;
+                bookingElementCopy.querySelector("#event_type").innerHTML =  "<b>Event Type: </b>" + booking.eventType;
+//                bookingElementCopy.querySelector("#duration").innerHTML =  "<b>Duration: </b>" + booking.duration + " hours";
+
+                let dateTime = new Date(booking.date);
+                const formattedDate = dateTime.toLocaleString('en-US', {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    hour12: false
+                });
+                bookingElementCopy.querySelector("#date").innerText = dateTime.toDateString() + ', ' + formattedDate;
+
+                let bookingState = bookingElementCopy.querySelector("#state");
+
+                switch (booking.state) {
+                    case "PENDING":
+                        bookingState.classList.add("pending-state");
+                        break;
+                    case "APPROVED":
+                        bookingState.classList.add("approved-state");
+                        break;
+                    default:
+                        bookingState.classList.add("pending-state");
+                }
+
+                bookingState.innerText = booking.state;
+                bookingContainer.appendChild(bookingElementCopy);
+
+            });
+        })
+        .catch(error => {
+            // Handle any errors
+            console.error(error);
+        });
+}
+
+
+function updateBookings(filterType) {
+    const filterButton = document.getElementById("filter_button_text");
+
+    var filter;
+
+    switch (filterType) {
+        case 'in_progress':
+            filter = "ongoing";
+            filterButton.textContent = "Filter: In progress";
+            break;
+        case 'future':
+            filter = "ongoing";
+            filterButton.textContent = "Filter: On going";
+            break;
+        case 'past':
+            filter = "past";
+            filterButton.textContent = "Filter: Past";
+            break;
+        default:
+            filter = 'ongoing';
+
+    }
+
+    const cookies = parseCookie(document.cookie);
+    const id = cookies['account_id'];
+
+    const url = `http://localhost:8080/shotmaniacs2/api/crew/${id}/mybooking/timefilter/${filter}`;
+
+    performQueryAndUpdateBookings(url);
+
+}
+
+function searchBookings(searchText) {
+    const cookies = parseCookie(document.cookie);
+    const id = cookies['account_id'];
+    const url = `http://localhost:8080/shotmaniacs2/api/crew/${id}/mybooking/search?searchtext=\"${searchText}\"`;
+    performQueryAndUpdateBookings(url);
+}
+
+//Event listeners
+const searchbox = document.getElementById("search-input");
+searchbox.addEventListener("keydown", function (e) {
+    if (e.code === "Enter") {  //checks whether the pressed key is "Enter"
+        searchBookings(searchbox.value);
+    }
+});
+
+updateBookings("ongoing");
