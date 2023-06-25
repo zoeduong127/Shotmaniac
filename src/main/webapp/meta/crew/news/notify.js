eb3b0bef15d27974e1677f5064e62231a29679b7
Thomas <f.t.vandervanderboon@student.utwente.nl>
2023-06-25 15:33:32 +0200

    notifications.html complete, just missing some images

--- src/main/webapp/meta/crew/news/notify.js
+++ src/main/webapp/meta/crew/news/notify.js
@@ -3,13 +3,14 @@ const announcementContent = document.querySelector(".announcement-content");
 const announcementTitle = document.querySelector("#announcement-title");
 const announcementDescription = document.querySelector("#announcement-description");
 const announcementList = document.querySelector('.announcement-list');
-
+const pre_announcement = document.getElementById('pre-announcement');
 const addCommentButton = document.getElementById("add-comment");
 const commentBox = document.getElementById("comment-box");
 const commentInput = document.getElementById("comment-input");
 const commentSubmitButton = document.getElementById("comment-submit");
 
 //clicking on an announcement makes it show the content
+/*
 announcementListItems.forEach(function(item) {
     item.addEventListener("click", function() {
         const title = this.textContent.trim();
@@ -19,9 +20,18 @@ announcementListItems.forEach(function(item) {
         announcementDescription.textContent = content;
         announcementContent.style.display = "block";
         addCommentButton.style.visibility = "visible";
+        preannouncement.style.visibility = "hidden"
     });
 });
+*/
 
+function display(element) {
+    announcementTitle.textContent = element.innerText;
+    announcementDescription.textContent = "";
+    announcementContent.style.display = "block";
+    addCommentButton.style.visibility = "visible";
+    pre_announcement.style.visibility = "hidden";
+}
 /*announcementContent.addEventListener('click', function() {
     announcementTitle.textContent = '';
     announcementDescription.textContent = '';
@@ -35,6 +45,8 @@ filterButton.addEventListener('click', function() {
     announcementList.classList.toggle('announcement-list-filtered');
 });
 
+
+
 filterOptions.forEach(function(option) {
     option.addEventListener('click', function(e) {
         e.preventDefault();
@@ -44,13 +56,17 @@ filterOptions.forEach(function(option) {
         // You can add your code here to filter the announcements based on the selected option
     });
 });
-
-
 function showComment() {
-    commentBox.style.visibility = "visible";
+    const comment = document.getElementById("add-comment");
+    if (comment.innerHTML === "Add a comment") {
+        comment.innerHTML = "Close comment box";
+        commentBox.style.visibility = "visible";
+    } else {
+        comment.innerHTML = "Add a comment";
+        commentBox.style.visibility = "hidden";
+    }
 }
-addCommentButton.addEventListener("click", function() {
-});
+
 
 commentSubmitButton.addEventListener("click", function() {
     const comment = commentInput.value.trim();
@@ -58,6 +74,7 @@ commentSubmitButton.addEventListener("click", function() {
         // Logic to handle the submitted comment
         console.log("Submitted comment:", comment);
         commentInput.value = "";
-        commentBox.style.display = "none";
+        const comment_info = document.getElementById("add-comment").innerHTML = "Add a comment";
+        commentBox.style.visibility = "hidden";
     }
 });
