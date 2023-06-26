32a02784f7172941e69caa644dd8ff17a347f636
Thomas <f.t.vandervanderboon@student.utwente.nl>
2023-06-26 12:17:31 +0200

    completed fetching for all bookings and I have a function that should return a single booking based on the id and fill in the slots needed but missing the backend section on my computer so unsure if it works

--- src/main/webapp/meta/Admin/news/notify.js
+++ src/main/webapp/meta/Admin/news/notify.js
@@ -3,8 +3,14 @@ const announcementContent = document.querySelector(".announcement-content");
 const announcementTitle = document.querySelector("#announcement-title");
 const announcementDescription = document.querySelector("#announcement-description");
 const announcementList = document.querySelector('.announcement-list');
+const pre_announcement = document.getElementById('pre-announcement');
+const addCommentButton = document.getElementById("add-comment");
+const commentBox = document.getElementById("comment-box");
+const commentInput = document.getElementById("comment-input");
+const commentSubmitButton = document.getElementById("comment-submit");
 
 //clicking on an announcement makes it show the content
+/*
 announcementListItems.forEach(function(item) {
     item.addEventListener("click", function() {
         const title = this.textContent.trim();
@@ -13,9 +19,19 @@ announcementListItems.forEach(function(item) {
         announcementTitle.textContent = title;
         announcementDescription.textContent = content;
         announcementContent.style.display = "block";
+        addCommentButton.style.visibility = "visible";
+        preannouncement.style.visibility = "hidden"
     });
 });
+*/
 
+function display(element) {
+    announcementTitle.textContent = element.innerText;
+    announcementDescription.textContent = "";
+    announcementContent.style.display = "block";
+    addCommentButton.style.visibility = "visible";
+    pre_announcement.style.visibility = "hidden";
+}
 /*announcementContent.addEventListener('click', function() {
     announcementTitle.textContent = '';
     announcementDescription.textContent = '';
@@ -29,6 +45,8 @@ filterButton.addEventListener('click', function() {
     announcementList.classList.toggle('announcement-list-filtered');
 });
 
+
+
 filterOptions.forEach(function(option) {
     option.addEventListener('click', function(e) {
         e.preventDefault();
@@ -38,15 +56,17 @@ filterOptions.forEach(function(option) {
         // You can add your code here to filter the announcements based on the selected option
     });
 });
+function showComment() {
+    const comment = document.getElementById("add-comment");
+    if (comment.innerHTML === "Add a comment") {
+        comment.innerHTML = "Close comment box";
+        commentBox.style.visibility = "visible";
+    } else {
+        comment.innerHTML = "Add a comment";
+        commentBox.style.visibility = "hidden";
+    }
+}
 
-const addCommentButton = document.getElementById("add-comment");
-const commentBox = document.getElementById("comment-box");
-const commentInput = document.getElementById("comment-input");
-const commentSubmitButton = document.getElementById("comment-submit");
-
-addCommentButton.addEventListener("click", function() {
-    commentBox.style.display = "block";
-});
 
 commentSubmitButton.addEventListener("click", function() {
     const comment = commentInput.value.trim();
@@ -54,27 +74,7 @@ commentSubmitButton.addEventListener("click", function() {
         // Logic to handle the submitted comment
         console.log("Submitted comment:", comment);
         commentInput.value = "";
-        commentBox.style.display = "none";
+        const comment_info = document.getElementById("add-comment").innerHTML = "Add a comment";
+        commentBox.style.visibility = "hidden";
     }
 });
-
-
-const addCommentButton = document.getElementById("add-comment");
-const commentBox = document.getElementById("comment-box");
-const commentInput = document.getElementById("comment-input");
-const commentSubmitButton = document.getElementById("comment-submit");
-
-addCommentButton.addEventListener("click", function() {
-    commentBox.style.display = "block";
-});
-
-commentSubmitButton.addEventListener("click", function() {
-    const comment = commentInput.value.trim();
-    if (comment !== "") {
-        // Logic to handle the submitted comment
-        console.log("Submitted comment:", comment);
-        commentInput.value = "";
-        commentBox.style.display = "none";
-    }
-});
-
--- src/main/webapp/meta/crew/bookings/bookings.js
+++ src/main/webapp/meta/crew/bookings/bookings.js
@@ -1,34 +1,129 @@
 const booking = document.getElementById("booking_container");
 const popup = document.getElementById("hiddenpop");
+const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
 
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
 
 function openInfo(element) {
     booking.style.opacity = "0.2";
     popup.style.visibility = "visible";
 
+    const cookies = parseCookie(document.cookie);
+    const id = cookies['account_id'];
 
-    /*TODO: is it best to compare to the full database or try something else*/
-    console.log(element.children[0]);
-    console.log(element.children[1]);
-    let info = "";
-    info += `
-            <img class="icon" src="">
-            <p>Still Hard Coded</p>
-            <img class="icon" src="">
-            <p>Still hard Coded</p>
-            <img class="icon" src="">
-            <p>${element.children[1].innerText}</p>
-            `;
+    const url = `http://localhost:8080/shotmaniacs2/api/crew/${id}/booking/${element.id}`
+    fetch(url)
+        .then(response => response.json())
+        .then(booking => {
+            let element = "";
+            /*TODO: is it best to compare to the full database or try something else*/
+            console.log(element.children[0]);
+            console.log(element.children[1]);
 
-    document.getElementById("top-tab").innerHTML = `<h4 id = "heading"> ${element.children[0].innerText} </h4>
-        <span id="back">
-                <img src="./../../Brand_Identity/images/icons/X-icon.png" alt="close" id="back-img" onclick="closeInfo()">
-        </span>`;
 
-    document.getElementById("popup-main").innerHTML = info;
+            let info = "";
+            info += `
+                    <img class="icon" src="">
+                    <p>${days[new Date(booking.date).getDay()]}, 
+                       ${new Date(booking.date).getDate()} 
+                       ${months[new Date(booking.date).getMonth()]} 
+                       ${new Date(booking.date).getFullYear()}
+                       ${new Date(booking.date).getTime()}
+                    </p>
+                    <img class="icon" src="">
+                    <p><strong>Available slots: </strong>${booking.slot}</p> <!-- TODO: subtract number of crew in the event -->
+                    <img class="icon" src="">
+                    <p>${booking.description}</p>
+                    `;
+
+            document.getElementById("top-tab").innerHTML = `<h4 id = "heading"> ${booking.name}</h4>
+                <span id="back">
+                        <img src="./../../Brand_Identity/images/icons/X-icon.png" alt="close" id="back-img" onclick="closeInfo()">
+                </span>`;
+
+            document.getElementById("popup-main").innerHTML = info;
+        })
 }
+
 
 function closeInfo() {
     booking.style.opacity = "1";
     popup.style.visibility = "hidden";
 }
+
+
+
+/*Cookies*/
+function parseCookie(cookieString) {
+    const cookies = {};
+    cookieString.split(';').forEach(cookie => {
+        const [name, value] = cookie.trim().split('=');
+        cookies[name] = decodeURIComponent(value);
+    });
+    return cookies;
+}
+
+
+/*Get all bookings*/
+
+function performQueryAndUpdateBookings() {
+
+    const cookies = parseCookie(document.cookie);
+    const id = cookies['account_id'];
+
+    const url = `http://localhost:8080/shotmaniacs2/api/crew/${id}/allbookings`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            let element = "";
+            data.forEach(booking => {
+                //TODO: Calculate amount of slots already taken.
+                console.log(booking);
+
+                element += `
+                    <div class="booking" id="${booking.id}" onclick="openInfo(this)">
+                        <div class="bookings-name">
+                    
+                            <p>${booking.name}</p>
+                    
+                        </div>
+                        <div class="short-descri">
+                            <p>${booking.description}</p>
+                        </div>
+                    </div>`;
+
+            });
+            document.getElementById("booking_container").innerHTML = element;
+        })
+        .catch(error => {
+            // Handle any errors
+            console.error(error);
+        });
+}
+
+
+
+/*To load at the start of the page*/
+performQueryAndUpdateBookings();
