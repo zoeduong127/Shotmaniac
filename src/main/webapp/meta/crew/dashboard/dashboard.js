bb283d4d3a8e8b5ff814577dd7303cdb6dc53032
Duonghuyen <duongthuhuyen@student.utwente.nl>
2023-06-30 01:56:00 +0200

    statistics done

--- src/main/webapp/meta/crew/dashboard/dashboard.js
+++ src/main/webapp/meta/crew/dashboard/dashboard.js
@@ -70,7 +70,7 @@ function addCrew(id) {
 
 
 function addProductManager(id) {
-    const url = `http://localhost:8080/shotmaniacs2/api/admin/booking/${id}/productmanager`
+    const url = window.location.origin+`/shotmaniacs2/api/admin/booking/${id}/productmanager`
 
     fetch(url, {
         headers: {
@@ -88,7 +88,7 @@ function addProductManager(id) {
 }
 
 function setLabel(id) {
-    const url = `http://localhost:8080/shotmaniacs2/api/crew/${account_id}/booking/${id}/label`;
+    const url = window.location.origin+`/shotmaniacs2/api/crew/${account_id}/booking/${id}/label`;
 
     fetch(url, {
         headers: {
@@ -120,7 +120,7 @@ function toggleStyleAndPage(element) {
     console.log('new theme changed to: ' + currentTheme.getAttribute('href'));
 
 
-    const url = `http://localhost:8080/shotmaniacs2/api/crew/${account_id}/booking/${element.id}`;
+    const url = window.location.origin+`/shotmaniacs2/api/crew/${account_id}/booking/${element.id}`;
 
     fetch(url, {
         headers: {
@@ -290,7 +290,7 @@ function addLabel(list) {
 
     for (let i = 0; i < list.length; i++) {
         let id = list[i];
-        const url = `http://localhost:8080/shotmaniacs2/api/crew/${account_id}/booking/${id}/label`;
+        const url = window.location.origin+`/shotmaniacs2/api/crew/${account_id}/booking/${id}/label`;
         fetch(url, {
             headers: {
                 'Authorization': `${token}`
@@ -329,43 +329,43 @@ function updateBookings(filterType) {
         case 'in_progress': {
             filter = "IN_PROGRESS";
             filterButton.textContent = "Filter: In progress";
-            url = `http://localhost:8080/shotmaniacs2/api/crew/${account_id}/mybooking/labelfilter/${filter}`;
+            url = window.location.origin+`/shotmaniacs2/api/crew/${account_id}/mybooking/labelfilter/${filter}`;
             break;
         }
         case "todo": {
             filter = "TODO";
             filterButton.textContent = "Filter: TODO";
-            url = `http://localhost:8080/shotmaniacs2/api/crew/${account_id}/mybooking/labelfilter/${filter}`;
+            url = window.location.origin+`/shotmaniacs2/api/crew/${account_id}/mybooking/labelfilter/${filter}`;
             break;
         }
         case "review": {
             console.log("Entered Review Section");
             filter = "REVIEW";
             filterButton.textContent = "Filter: REVIEW";
-            url = `http://localhost:8080/shotmaniacs2/api/crew/${account_id}/mybooking/labelfilter/REVIEW`;
+            url = window.location.origin+`/shotmaniacs2/api/crew/${account_id}/mybooking/labelfilter/REVIEW`;
             break;
         }
         case "done": {
             filter = "DONE";
             filterButton.textContent = "Filter: DONE";
-            url = `http://localhost:8080/shotmaniacs2/api/crew/${account_id}/mybooking/labelfilter/${filter}`;
+            url = window.location.origin+`/shotmaniacs2/api/crew/${account_id}/mybooking/labelfilter/${filter}`;
             break;
         }
         case 'future': {
             filter = "ongoing";
             filterButton.textContent = "Filter: Upcoming";
-            url = `http://localhost:8080/shotmaniacs2/api/crew/${account_id}/mybooking/timefilter/${filter}`;
+            url = window.location.origin+`/shotmaniacs2/api/crew/${account_id}/mybooking/timefilter/${filter}`;
             break;
         }
         case 'past': {
             filter = "past";
             filterButton.textContent = "Filter: Past";
-            url = `http://localhost:8080/shotmaniacs2/api/crew/${account_id}/mybooking/timefilter/${filter}`;
+            url = window.location.origin+`/shotmaniacs2/api/crew/${account_id}/mybooking/timefilter/${filter}`;
             break;
         }
         default: {
             filterButton.textContent = "All Bookings";
-            url = `http://localhost:8080/shotmaniacs2/api/crew/${account_id}/mybookings`;
+            url = window.location.origin+`/shotmaniacs2/api/crew/${account_id}/mybookings`;
         }
     }
 
@@ -416,7 +416,7 @@ function updateLabel(label, id) {
     }
 
 
-    const url = `http://localhost:8080/shotmaniacs2/api/crew/${account_id}/booking/${id}/label?label=${label.innerText.replace("-"," ").toUpperCase()}`;
+    const url = window.location.origin +`/shotmaniacs2/api/crew/${account_id}/booking/${id}/label?label=${label.innerText.replace("-"," ").toUpperCase()}`;
     fetch(url, {
             method: 'PUT',
             headers: {
@@ -444,7 +444,7 @@ let inputElement = document.getElementById("search-input");
 
 inputElement.addEventListener("input", onInputChange);
 function getAllBookings() {
-    const url = `http://localhost:8080/shotmaniacs2/api/crew/${account_id}/mybookings`
+    const url = window.location.origin+`/shotmaniacs2/api/crew/${account_id}/mybookings`
 
     fetch(url, {
         headers: {
@@ -519,7 +519,7 @@ function sendInput(event) {
 
     if (inputElement.value.length === 0) updateBookings("");
     else {
-        const url = `http://localhost:8080/shotmaniacs2/api/crew/${account_id}/mybooking/search?searchtext="${inputElement.value}"`
+        const url = window.location.origin+`/shotmaniacs2/api/crew/${account_id}/mybooking/search?searchtext="${inputElement.value}"`
         performQueryAndUpdateBookings(url);
     }
 }
@@ -554,4 +554,80 @@ function logout(){
             console.error(error);
         });
 
+}
+function graph(){
+    const filter = "DONE";
+    const url = window.location.origin+`/shotmaniacs2/api/crew/${account_id}/mybooking/labelfilter/${filter}`;
+    fetch(url, {
+        headers: {
+            'Authorization': `${token}`
+        }
+    })
+        .then(response => response.json())
+        .then(bookings => {
+            var bookingCounts = {};
+
+            bookings.forEach(function (booking) {
+                var timestamp = new Date(parseInt(booking.date));
+
+                if (!isNaN(timestamp)) { // Check if timestamp is valid
+                    var date = new Date(timestamp); // Convert to Date object
+
+                    var monthh = date.getFullYear() + '-' + (date.getMonth() + 1).toString().padStart(2, '0');
+
+                    if (bookingCounts[monthh]) {
+                        bookingCounts[monthh]++;
+                    } else {
+                        bookingCounts[monthh] = 1;
+                    }
+                }});
+
+            // Convert the booking counts object to arrays
+            var monthss = Object.keys(bookingCounts);
+            var counts = monthss.map(function(month) {
+                return bookingCounts[month];
+            });
+
+            // Get the canvas element
+            var canvas = document.getElementById('chart');
+            var ctx = canvas.getContext('2d');
+
+            // Chart configuration
+            var chartConfig = {
+                type: 'line',
+                data: {
+                    labels: monthss,
+                    datasets: [{
+                        label: 'Number of Bookings',
+                        data: counts,
+                        borderColor: 'blue',
+                        backgroundColor: 'transparent'
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    title: {
+                        display: true,
+                    },
+                    scales: {
+                        x: {
+                            display: true,
+                            title: {
+                                display: true,
+                            },
+                            min: monthss[0],  // Specify the minimum value on the x-axis
+                            max: monthss[4]
+                        },
+                        y: {
+                            display: true,
+                            title: {
+                                display: true,
+                            },
+                        }
+                    }
+                }
+            };
+
+            // Create the chart graph
+            var chart = new Chart(ctx, chartConfig);})
 }
